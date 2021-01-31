f2dea1f76257a85846c2751179d0231a8d7f42f3
zergon321 <maximgradan@gmail.com>
2021-01-31 17:09:10 +0000

    Buffer size manipulation added.

--- queue.go
+++ queue.go
@@ -1,8 +1,42 @@
 package arrayqueue
 
+import "fmt"
+
 // Queue is a queue based on a slice.
 type Queue struct {
 	data []interface{}
+}
+
+// BufferSize returns the capacity of the queue.
+func (queue *Queue) BufferSize() int {
+	return cap(queue.data)
+}
+
+// SetBufferSize sets the capacity of the queue.
+func (queue *Queue) SetBufferSize(bufferSize int) error {
+	if bufferSize < 0 {
+		return fmt.Errorf(
+			"buffer size is less than 0: %d", bufferSize)
+	}
+
+	if bufferSize < len(queue.data) {
+		return fmt.Errorf(
+			"buffer size is less than the length of the queue: %d",
+			bufferSize)
+	}
+
+	data := make([]interface{}, bufferSize)
+	data = data[:0]
+
+	copy(data, queue.data)
+	queue.data = data
+
+	return nil
+}
+
+// Clear removes all the elements from the queue.
+func (queue *Queue) Clear() {
+	queue.data = queue.data[:0]
 }
 
 // Length returns the numbers of elements
@@ -31,15 +65,18 @@ func (queue *Queue) Dequeue() interface{} {
 	lastIdx := len(queue.data) - 1
 	lastElem := queue.data[lastIdx]
 
-	queue.data = append(queue.data[:lastIdx])
+	queue.data = queue.data[:lastIdx]
 
 	return lastElem
 }
 
 // NewQueue returns a new queue
 // based on a slice.
-func NewQueue() *Queue {
+func NewQueue(queueCapacity int) *Queue {
+	data := make([]interface{}, queueCapacity)
+	data = data[:0]
+
 	return &Queue{
-		data: []interface{}{},
+		data: data,
 	}
 }
